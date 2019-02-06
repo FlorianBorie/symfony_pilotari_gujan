460704d4d5ef031067255df17ec62d5e631a8423
Florian16100 <florian.lys@ynov.com>
2019-02-06 10:30:49 +0100

    ok

--- site/src/Controller/SiteController.php
+++ site/src/Controller/SiteController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SiteController extends AbstractController
@@ -20,13 +21,16 @@ class SiteController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function home(){
+    public function home()
+    {
         return $this->render(('site/home.html.twig'));
     }
+
     /**
      * @Route("/evenement", name="evenement")
      */
-    public function evenement(){
+    public function evenement()
+    {
         return $this->render('site/evenement.html.twig');
     }
 
@@ -34,7 +38,8 @@ class SiteController extends AbstractController
      * @Route("/competition", name="competition")
      */
 
-    public function competion(){
+    public function competion()
+    {
         return $this->render('site/competition.html.twig');
     }
 
@@ -42,28 +47,35 @@ class SiteController extends AbstractController
      * @Route("/sponsor", name="sponsor")
      */
 
-    public function sponsor(){
+    public function sponsor()
+    {
         return $this->render('site/sponsor.html.twig');
     }
+
     /**
      * @Route("/newsletter", name="newsletter")
      */
 
-    public function newsletter(){
+    public function newsletter()
+    {
         return $this->render('site/newsletter.html.twig');
     }
+
     /**
      * @Route("/connexion", name="connexion")
      */
 
-    public function connexion(){
+    public function connexion()
+    {
         return $this->render('site/connexion.html.twig');
     }
+
     /**
      * @Route("/inscription", name="inscription")
      */
 
-    public function inscription(){
+    public function inscription()
+    {
         return $this->render('site/inscription.html.twig');
     }
 }
